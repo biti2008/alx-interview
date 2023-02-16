669dc76fe5b8a0068e1f180e6b026cb5fda74ec8
biti2008 <tesfaybrkti00@gmail.com>
2023-02-16 17:32:24 +0300

    My commit

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""Rotates 2-D matrix"""
+'''2D matrix'''
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates in-place"""
-    row = len(matrix)
-    column = row - 1
+    '''rotates a 2d matrix 90° clockwise
+    Returns: Nothing'''
+    left, right = 0, len(matrix) - 1
 
-    for i in range(0, int(row / 2)):
-        for j in range(i, column - i):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[column - j][i]
-            matrix[column - j][i] = matrix[column - i][column - j]
-            matrix[column - i][column - j] = matrix[j][column - i]
-            matrix[j][column - i] = temp
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
